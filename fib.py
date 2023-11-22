6d6014067c1cdc765e0e4f474e5388d9dba8eb50
YejinKang <ks990528@naver.com>
2023-11-22 22:01:23 +0900

    no

--- fib.py
+++ fib.py
@@ -6,14 +6,9 @@ The zeroth number in the fibonacci sequence is 0. The first number is 1
 Negative numbers should return None
 """
 def fibonacci(position):
-  if position < 0:
-    return None
-  elif position == 0:
-    return 0
-  elif position == 1:
+  if(position == 1 or position == 2):
     return 1
-  else:
-    return fibonacci(position - 1) + fibonacci(position - 2)
+  return fibonacci(position - 1) + fibonacci(position - 2)
 
 
 
